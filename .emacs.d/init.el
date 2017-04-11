b199a2c78d65df94a67678d548cfac6237cff795
Joseph Wright <rjosephwright@gmail.com>
2017-04-10 21:06:49 -0700

    Removing ponylang-mode, which I was only checking out

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -123,16 +123,6 @@
   :config
   (global-set-key (kbd "M-x") 'smex))
 
-(use-package ponylang-mode
-  :ensure t
-  :config
-  (progn
-    (add-hook
-     'ponylang-mode-hook
-     (lambda ()
-       (set-variable 'indent-tabs-mode nil)
-       (set-variable 'tab-width 2)))))
-
 (use-package geiser
   :ensure t
   :config
