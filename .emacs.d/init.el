fe132c29c36856c6b61675e23cdd1ab631e2e4dd
Joseph Wright <rjosephwright@gmail.com>
2019-08-04 15:54:27 -0400

    Remove marmalade Emacs repo due to expired certificate

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,7 +29,6 @@
 
 ;; Elpa
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 ;; Install desired packages automatically
