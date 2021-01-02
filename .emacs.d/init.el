4ecfabca6533c5a39b17af15e728762357e24665
Joseph Wright <rjosephwright@gmail.com>
2021-01-02 15:19:50 -0500

    Update Rust Racer source path

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -323,7 +323,7 @@
   (add-hook 'rust-mode-hook #'racer-mode)
   (add-hook 'racer-mode-hook #'eldoc-mode)
   (add-hook 'racer-mode-hook #'company-mode)
-  (setq racer-rust-src-path "~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"))
+  (setq racer-rust-src-path "~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library"))
 
 ;; Groovy
 (use-package groovy-mode
