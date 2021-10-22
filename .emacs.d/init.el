e747a3a218b118a0e8eb311a635d21b84eace00c
Joseph Wright <rjosephwright@gmail.com>
2021-10-22 00:35:56 -0400

    Remove obsolete color-theme and unused sublime-themes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,7 +17,7 @@
  '(fill-column 80)
  '(js-indent-level 2)
  '(package-selected-packages
-   '(rustic lsp-ui omnisharp lsp-pyright toml-mode haskell-mode company hover racket-mode lsp-haskell lsp-mode yasnippet-snippets company-terraform terraform-doc elm-mode reason-mode zig-mode sphinx-frontend sphinx-mode quack go-projectile flx-ido projectile ido-completing-read+ yasnippet terraform-mode a groovy-mode sbt-mode clojure-mode elixir-mode dracula-theme adoc-mode auto-virtualenv pyvenv hindent haskell-snippets racer intero yaml-mode web-mode vagrant-tramp use-package sublime-themes smex rust-mode paredit markdown-mode magit go-snippets go-mode go-autocomplete geiser fold-this flycheck-rust exec-path-from-shell company-ghci company-cabal color-theme cider better-defaults alchemist))
+   '(rustic lsp-ui omnisharp lsp-pyright toml-mode haskell-mode company hover racket-mode lsp-haskell lsp-mode yasnippet-snippets company-terraform terraform-doc elm-mode reason-mode zig-mode sphinx-frontend sphinx-mode quack go-projectile flx-ido projectile ido-completing-read+ yasnippet terraform-mode a groovy-mode sbt-mode clojure-mode elixir-mode dracula-theme adoc-mode auto-virtualenv pyvenv hindent haskell-snippets racer intero yaml-mode web-mode vagrant-tramp use-package smex rust-mode paredit markdown-mode magit go-snippets go-mode go-autocomplete geiser fold-this flycheck-rust exec-path-from-shell company-ghci company-cabal cider better-defaults alchemist))
  '(safe-local-variable-values '((encoding . utf-8)))
  '(tramp-default-method "scpx"))
 (custom-set-faces
@@ -37,14 +37,12 @@
 (defvar package-list
   '(async
     better-defaults
-    color-theme
     dash
     f
     magit
     markdown-mode
     queue
     sbt-mode
-    sublime-themes
     use-package
     vagrant-tramp
     yaml-mode))
