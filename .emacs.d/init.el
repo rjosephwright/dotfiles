9df23b6c8a8e29089fff1a19220e7ba2bc0b9edd
Joseph Wright <rjosephwright@gmail.com>
2021-12-30 22:33:35 -0500

    Move emacs custom-set-variables into their own file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,29 +3,10 @@
 ;;; Commentary:
 ;; My not so über Emacs init
 
-(if (file-exists-p "~/.emacs.d/init/env.el")
-    (load "~/.emacs.d/init/env"))
+;; Custom set variables go here.
+(load (concat user-emacs-directory "custom.el") 'noerror)
 
 ;;; Code:
-;; Variables set via customize
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(byte-compile-warnings '(cl-functions))
- '(fill-column 80)
- '(js-indent-level 2)
- '(package-selected-packages
-   '(rustic lsp-ui omnisharp lsp-pyright toml-mode haskell-mode company hover racket-mode lsp-haskell lsp-mode yasnippet-snippets company-terraform terraform-doc elm-mode reason-mode zig-mode sphinx-frontend sphinx-mode quack go-projectile flx-ido projectile ido-completing-read+ yasnippet terraform-mode a groovy-mode sbt-mode clojure-mode elixir-mode dracula-theme adoc-mode auto-virtualenv pyvenv hindent haskell-snippets racer intero yaml-mode web-mode vagrant-tramp use-package smex rust-mode paredit markdown-mode magit go-snippets go-mode go-autocomplete geiser fold-this flycheck-rust exec-path-from-shell company-ghci company-cabal cider better-defaults alchemist))
- '(safe-local-variable-values '((encoding . utf-8)))
- '(tramp-default-method "scpx"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background nil)))))
 
 ;; Elpa
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
